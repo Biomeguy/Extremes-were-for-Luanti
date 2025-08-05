dd656d2d7e50f3f92e13f379fa720a836e2bb4ab
Biomeguy <45724011+Biomeguy@users.noreply.github.com>
2025-08-05 00:18:04 -0500

    Add files via upload

---
+++ games/isolus/mods/carts/rails.lua
@@ -0,0 +1,83 @@
+-- carts/rails.lua
+
+-- support for MT game translation.
+local S = carts.get_translator
+
+carts:register_rail("carts:rail", {
+	description = S("Rail"),
+	tiles = {
+		"carts_rail_straight.png", "carts_rail_curved.png",
+		"carts_rail_t_junction.png", "carts_rail_crossing.png"
+	},
+	inventory_image = "carts_rail_straight.png",
+	wield_image = "carts_rail_straight.png",
+	groups = carts:get_rail_groups(),
+}, {})
+
+core.register_craft({
+	output = "carts:rail 18",
+	recipe = {
+		{"default:steel_ingot", "group:wood", "default:steel_ingot"},
+		{"default:steel_ingot", "", "default:steel_ingot"},
+		{"default:steel_ingot", "group:wood", "default:steel_ingot"},
+	}
+})
+
+core.register_alias("default:rail", "carts:rail")
+
+
+carts:register_rail("carts:powerrail", {
+	description = S("Powered Rail"),
+	tiles = {
+		"carts_rail_straight_pwr.png", "carts_rail_curved_pwr.png",
+		"carts_rail_t_junction_pwr.png", "carts_rail_crossing_pwr.png"
+	},
+	groups = carts:get_rail_groups(),
+}, {acceleration = 5})
+
+core.register_craft({
+	output = "carts:powerrail 18",
+	recipe = {
+		{"default:steel_ingot", "group:wood", "default:steel_ingot"},
+		{"default:steel_ingot", "default:mese_crystal", "default:steel_ingot"},
+		{"default:steel_ingot", "group:wood", "default:steel_ingot"},
+	}
+})
+
+
+carts:register_rail("carts:brakerail", {
+	description = S("Brake Rail"),
+	tiles = {
+		"carts_rail_straight_brk.png", "carts_rail_curved_brk.png",
+		"carts_rail_t_junction_brk.png", "carts_rail_crossing_brk.png"
+	},
+	groups = carts:get_rail_groups(),
+}, {acceleration = -3})
+
+core.register_craft({
+	output = "carts:brakerail 18",
+	recipe = {
+		{"default:steel_ingot", "group:wood", "default:steel_ingot"},
+		{"default:steel_ingot", "default:coal_lump", "default:steel_ingot"},
+		{"default:steel_ingot", "group:wood", "default:steel_ingot"},
+	}
+})
+
+
+carts:register_rail("carts:copper_rail", {
+	description = S("Copper Rail"),
+	tiles = {
+		"carts_rail_copper.png", "carts_rail_copper_curved.png",
+		"carts_rail_copper_t_junction.png", "carts_rail_copper_crossing.png"
+	},
+	groups = carts:get_rail_groups(),
+}, {})
+
+core.register_craft({
+	output = "carts:copper_rail 18",
+	recipe = {
+		{"default:copper_ingot", "group:wood", "default:copper_ingot"},
+		{"default:copper_ingot", "", "default:copper_ingot"},
+		{"default:copper_ingot", "group:wood", "default:copper_ingot"}
+	}
+})
