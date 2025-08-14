f568f782d39528c65953486c87f0b35d7064c839
Biomeguy <45724011+Biomeguy@users.noreply.github.com>
2025-08-14 17:35:19 -0500

    Add files via upload

---
+++ Luanti-5.10.0-win64/mods/mobs_modpack/mobs_monster/fire_spirit.lua
@@ -0,0 +1,122 @@
+-- Translation support
+local S = core.get_translator("mobs_monster")
+
+local mob_drops = {
+	{name = "fireflies:firefly", chance = 1, min = 1, max = 1}
+}
+
+if core.get_modpath("ethereal") then
+
+	table.insert(mob_drops,
+			{name = "ethereal:fire_dust", chance = 1, min = 1, max = 1})
+end
+
+-- Fire Spirit
+
+local lp = core.settings:get_bool("limit_particles")
+
+mobs:register_mob("mobs_monster:fire_spirit", {
+	type = "monster",
+	passive = false,
+	attack_type = "dogfight",
+	pathfinding = true,
+	reach = 2,
+	damage = 4,
+	hp_min = 25,
+	hp_max = 45,
+	armor = 100,
+	collisionbox = {-.1, -.4, -.1, .1, .1, .1},
+	visual_scale = {x = .5, y = .5, z = .5},
+	visual = "sprite",
+	textures = {
+		{"mobs_fire_spirit.png"}
+	},
+	glow = 14,
+	blood_texture = "fire_basic_flame.png",
+	immune_to = {
+		{"bucket:bucket_water", 1},
+		{"bucket:bucket_fresh_water", 1},
+		{"bucket:clay_bucket_water", 1},
+		{"bucket:clay_bucket_fresh_water", 1},
+		{"bucket:wooden_bucket_water", 1},
+		{"bucket:wooden_bucket_fresh_water", 1},
+		{"all"}
+	},
+	makes_footstep_sound = false,
+	sounds = {
+		random = "fire_fire",
+		damage = "fire_extinguish_flame",
+		death = "fire_extinguish_flame"
+	},
+	view_range = 14,
+	walk_velocity = 2,
+	run_velocity = 3,
+	jump = true,
+	jump_height = 6,
+	drops = mob_drops,
+	water_damage = 1,
+	lava_damage = 0,
+	fire_damage = 0,
+	light_damage = 0,
+	fall_damage = false,
+	fear_height = 8,
+	animation = {},
+
+	on_die = function(self, pos)
+
+		mobs:effect(pos, 20, "tnt_smoke.png", 3, 5, 2, 0.5, nil, false)
+
+		self.object:remove()
+	end,
+
+	do_custom = function(self, dtime)
+
+		self.flame_timer = (self.flame_timer or 0) + dtime
+
+		if self.flame_timer < 0.25 then
+			return
+		end
+
+		self.flame_timer = 0
+
+		local pos = self.object:get_pos()
+
+		-- pos, amount, texture, min_size, max_size, radius, gravity, glow, fall
+		mobs:effect(pos, 5, "fire_basic_flame.png", 1, 2, 0.1, 0.2, 14, nil)
+		if self.state == "attack" and lp ~= true then-- attacking burst
+	for i = 1, 5 do
+core.add_particlespawner({
+	amount = 25,
+	time = 1,
+	minpos = vector.subtract(pos, .3),
+	maxpos = vector.add(pos, .3),
+	minvel = {x=-1, y=-1, z=-1},
+	maxvel = {x=1, y=1, z=1},
+	exptime = {min = .5, max = 1},
+	minsize = 1,
+	maxsize = 2,
+	collisiondetection = true,
+	texture = "fs_anger_".. i ..".png",
+	glow = 12 })
+	end
+		end
+	end
+})
+
+
+if not mobs.custom_spawn_monster then
+
+	mobs:spawn({
+		name = "mobs_monster:fire_spirit",
+		nodes = {"default:obsidian", "caverealms:hot_cobble"},
+		neighbors = {"group:fire"},
+		min_light = 12,
+		max_light = 15,
+		chance = 1500,
+		active_object_count = 1,
+		max_height = -150
+	})
+end
+
+
+mobs:register_egg("mobs_monster:fire_spirit", S("Fire Spirit"), "fire_basic_flame.png", 1)
